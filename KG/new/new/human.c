2fecb1deffc1430cdf11ffea31ff8bc2f8bd785d
ayymeredova <ayjahanmeredova17@mail.ru>
2021-02-17 21:56:47 +0300

    Update

--- KG/new/new/human.c
+++ KG/new/new/human.c
@@ -1,3 +1,8 @@
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
+Assimp::Importer importer;
+const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
 
